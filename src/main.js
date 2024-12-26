4dd00c632c1d0d0e3dbb1f79e9e68f1f9d7d6fbe
zjx23912 <zhangjixuan2022@163.com>
2024-12-26 21:34:18 +0800

    配置Axios

--- src/main.js
+++ src/main.js
@@ -2,5 +2,12 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from 'axios';
 
 createApp(App).mount('#app')
+
+axios.defaults.baseURL = 'http://localhost:8080'; // 替换为你的本地后端 API URL 和端口号
+
+app.config.globalProperties.$axios = axios;
+
+app.mount('#app');
